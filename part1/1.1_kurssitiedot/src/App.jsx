f89c8824ca8408bf43916925851e131955098fde
JerryU <jerry.uusitalo@gmail.com>
2023-10-30 22:11:55 +0200

    Fix

--- part1/1.1_kurssitiedot/src/App.jsx
+++ part1/1.1_kurssitiedot/src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
     return (
       <div>
         <Part part={part1} exercises = {exercises1}/>
-        <Part part={part1} exercises = {exercises1}/>
-        <Part part={part1} exercises = {exercises1}/>
+        <Part part={part2} exercises = {exercises2}/>
+        <Part part={part3} exercises = {exercises3}/>
       </div>
     )
   }
