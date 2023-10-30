d6f7afb6c60365e952acb6c69d66a216f027875d
JerryU <jerry.uusitalo@gmail.com>
2023-10-30 22:06:49 +0200

    Finish exercise 1.2

--- part1/1.1_kurssitiedot/src/App.jsx
+++ part1/1.1_kurssitiedot/src/App.jsx
@@ -16,15 +16,25 @@ const App = () => {
     )
   }
 
-  const Content = (props) => {
+  const Part = (props) => {
     return (
       <p>{props.part} {props.exercises}</p>
     )
   }
 
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercises = {exercises1}/>
+        <Part part={part1} exercises = {exercises1}/>
+        <Part part={part1} exercises = {exercises1}/>
+      </div>
+    )
+  }
+
   const Total = (props) => {
     return (
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
     )
   }
 
@@ -32,9 +42,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
