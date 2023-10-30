91294ea3c1d047d5f633d09ccb1d0e218f4238a7
JerryU <jerry.uusitalo@gmail.com>
2023-10-30 21:53:29 +0200

    Kurssitiedot 1.1 complete

---
+++ part1/1.1_kurssitiedot/src/App.jsx
@@ -0,0 +1,43 @@
+// Refaktoroi sovelluksen koodi siten, että se koostuu kolmesta uudesta komponentista: Header, Content ja Total. 
+// Kaikki data pidetään edelleen komponentissa App, joka välittää tarpeelliset tiedot kullekin komponentille props:ien avulla. Header huolehtii kurssin nimen renderöimisestä, Content osista ja niiden tehtävämääristä ja Total tehtävien yhteismäärästä.
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <p>{props.part} {props.exercises}</p>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    )
+  }
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
