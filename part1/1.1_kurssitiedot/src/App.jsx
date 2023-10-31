f6203f05284794a1734bb446a473a41b5eac5859
JerryU <jerry.uusitalo@gmail.com>
2023-10-31 16:40:04 +0200

    src/App.jsx
    Complete 1.4

--- part1/1.1_kurssitiedot/src/App.jsx
+++ part1/1.1_kurssitiedot/src/App.jsx
@@ -1,27 +1,24 @@
-// Refaktoroi sovelluksen koodi siten, että se koostuu kolmesta uudesta komponentista: Header, Content ja Total. 
-// Kaikki data pidetään edelleen komponentissa App, joka välittää tarpeelliset tiedot kullekin komponentille props:ien avulla. Header huolehtii kurssin nimen renderöimisestä, Content osista ja niiden tehtävämääristä ja Total tehtävien yhteismäärästä.
-
 import Part from "./Part";
 import Header from "./Header";
 import Total from "./Total";
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = { name: 'Fundamentals of React', exercises: 10 }
 
-  const part2 = { name: 'Using props to pass data', exercises: 7 }
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
 
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+
 
   const Content = () => {
     return (
       <div>
-        <Part part={part1.name} exercises={part1.exercises} />
-        <Part part={part2.name} exercises={part2.exercises} />
-        <Part part={part3.name} exercises={part3.exercises} />
+        <Part part={parts[0].name} exercises={parts[0].exercises} />
+        <Part part={parts[1].name} exercises={parts[1].exercises} />
+        <Part part={parts[2].name} exercises={parts[2].exercises} />
       </div>
     )
   }
@@ -30,7 +27,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </div>
   )
 }
