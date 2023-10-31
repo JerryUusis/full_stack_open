946764e03b92bd93e1051d9ccff6cabe71c61105
JerryU <jerry.uusitalo@gmail.com>
2023-10-31 15:54:10 +0200

    Add separte files for components and add imports and export commands

--- part1/1.1_kurssitiedot/src/App.jsx
+++ part1/1.1_kurssitiedot/src/App.jsx
@@ -1,6 +1,10 @@
 // Refaktoroi sovelluksen koodi siten, että se koostuu kolmesta uudesta komponentista: Header, Content ja Total. 
 // Kaikki data pidetään edelleen komponentissa App, joka välittää tarpeelliset tiedot kullekin komponentille props:ien avulla. Header huolehtii kurssin nimen renderöimisestä, Content osista ja niiden tehtävämääristä ja Total tehtävien yhteismäärästä.
 
+import Part from "./Part";
+import Header from "./Header";
+import Total from "./Total";
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -10,34 +14,15 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  const Header = (props) => {
-    return (
-      <h1>{props.course}</h1>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <p>{props.part} {props.exercises}</p>
-    )
-  }
-
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercises = {exercises1}/>
-        <Part part={part2} exercises = {exercises2}/>
-        <Part part={part3} exercises = {exercises3}/>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
       </div>
     )
   }
-
-  const Total = (props) => {
-    return (
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    )
-  }
-
 
   return (
     <div>
