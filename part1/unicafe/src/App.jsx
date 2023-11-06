3d6123f023d364eadb724c7eb21585d1fe65964b
JerryU <jerry.uusitalo@gmail.com>
2023-11-06 17:05:52 +0200

    Add placeholder components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import Heading from './Title'
+import Button from './Button'
+import Statistics from './Statistics';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <>
-
+      <Heading text ="Give feedback"/>
+      <div>
+        <Button text="Good"/>
+        <Button text="Neutral"/>
+        <Button text="Bad"/>
+      </div>
+      <Heading text ="Statistics"/> 
+      <Statistics reviewType= "Good" review = {6} />
+      <Statistics reviewType= "Neutral" review = {6} />
+      <Statistics reviewType= "Bad" review = {6} />
     </>
   )
 }
---
+++ part1/unicafe/src/Button.jsx
@@ -0,0 +1,9 @@
+const Button = (props) => {
+    return (
+        <>
+        <button>{props.text}</button>
+        </>
+    )
+}
+
+export default Button
