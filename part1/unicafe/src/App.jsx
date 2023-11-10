e1427a26cb18a08bc4e6ad9df3c8a4f2d5ad485d
JerryU <jerry.uusitalo@gmail.com>
2023-11-10 08:50:05 +0200

    Complete 1.9 & 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,26 +3,35 @@ import Heading from './Title'
 import Button from './Button'
 
 
-const StatisticsLine = ({reviewType, review}) => {
+const StatisticsLine = ({ reviewType, review }) => {
   return (
-      <>
-          <p>{reviewType} {review}</p>
-      </>
+    <tr>
+      <td>
+        <p>{reviewType}</p>
+      </td>
+      <td>
+        <p>{review}</p>
+      </td>
+    </tr>
   )
 }
 
-const Statistics = ({good, neutral, bad, total, calculateAverage, positivePercentage}) => {
-  if(total === 0) {
+const Statistics = ({ good, neutral, bad, total, calculateAverage, positivePercentage }) => {
+  if (total === 0) {
     return <p>No feedback given</p>
   }
   return (
     <div>
-      <StatisticsLine reviewType = "Good" review = {good} />
-      <StatisticsLine reviewType = "Neutral" review = {neutral} />
-      <StatisticsLine reviewType = "Bad" review = {bad} />
-      <StatisticsLine reviewType = "Total" review = {total} />
-      <StatisticsLine reviewType = "Average" review = {calculateAverage()} />
-      <StatisticsLine reviewType = "Positive" review = {positivePercentage()} />
+      <table>
+        <tbody>
+          <StatisticsLine reviewType="Good" review={good} />
+          <StatisticsLine reviewType="Neutral" review={neutral} />
+          <StatisticsLine reviewType="Bad" review={bad} />
+          <StatisticsLine reviewType="Total" review={total} />
+          <StatisticsLine reviewType="Average" review={calculateAverage()} />
+          <StatisticsLine reviewType="Positive" review={positivePercentage()} />
+        </tbody>
+      </table>
     </div>
   )
 
@@ -30,44 +39,44 @@ const Statistics = ({good, neutral, bad, total, calculateAverage, positivePercen
 
 function App() {
 
-const [feedback, setFeedback] = useState({
-  good: 0,
-  neutral: 0,
-  bad: 0
-})
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
 
-const handleFeedbackClick = (type) => {
-  setFeedback({ ...feedback, [type]: feedback[type] + 1})
-}
+  const handleFeedbackClick = (type) => {
+    setFeedback({ ...feedback, [type]: feedback[type] + 1 })
+  }
 
-const {good, neutral, bad} = feedback;
-const total = good + neutral + bad;
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
 
-const calculateAverage = () => {
-  return ((good * 1) + (neutral * 0) + (bad * -1)) / total; 
-}
+  const calculateAverage = () => {
+    return ((good * 1) + (neutral * 0) + (bad * -1)) / total;
+  }
 
-const positivePercentage = () => {
-  return `${(good / total) * 100} %`;
-}
+  const positivePercentage = () => {
+    return `${(good / total) * 100} %`;
+  }
 
   return (
     <>
-      <Heading text ="Give feedback"/>
+      <Heading text="Give feedback" />
       <div>
-        <Button text="Good" onClick={() => handleFeedbackClick('good')}/>
-        <Button text="Neutral" onClick={() => handleFeedbackClick('neutral')}/>
-        <Button text="Bad" onClick={() => handleFeedbackClick('bad')}/>
+        <Button text="Good" onClick={() => handleFeedbackClick('good')} />
+        <Button text="Neutral" onClick={() => handleFeedbackClick('neutral')} />
+        <Button text="Bad" onClick={() => handleFeedbackClick('bad')} />
       </div>
-      <Heading text ="Statistics"/> 
-      <Statistics 
-      good={good}
-      neutral={neutral}
-      bad={bad}
-      total={total}
-      calculateAverage={calculateAverage}
-      positivePercentage={positivePercentage} />
-      </>
+      <Heading text="Statistics" />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        calculateAverage={calculateAverage}
+        positivePercentage={positivePercentage} />
+    </>
   )
 }
 
